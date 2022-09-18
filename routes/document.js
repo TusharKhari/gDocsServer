0662209bff02eddff7e47d936f22f2b2f2bda7bb
TusharKhari <tusharkhari1@gmail.com>
2022-09-18 20:02:01 +0530

    get document by id

--- routes/document.js
+++ routes/document.js
@@ -46,4 +46,16 @@ documentRouter.post("/doc/title", auth , async (req, res)=>{
     }
 });
 
+
+documentRouter.get("/doc/:id", auth, async(req, res)=> {
+    try {
+        const document = await Document.findById(
+            req.params.id
+        ); 
+        res.json(document); 
+    } catch (e) {
+        res.status(500).json({error : e.message});  
+    }
+});
+
 module.exports = documentRouter;
